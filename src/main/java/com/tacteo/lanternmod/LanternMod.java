05a0337edc9e50c7f20a304af6f14df3d83e1508
Chad Magnuson <chadmagnuson@Chads-MacBook-Pro.local>
2018-04-02 16:57:00 -0500

    Initial project files.

---
+++ src/main/java/com/tacteo/lanternmod/LanternMod.java
@@ -0,0 +1,30 @@
+package com.tacteo.lanternmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = LanternMod.MODID, name = LanternMod.NAME, version = LanternMod.VERSION)
+public class LanternMod {
+    public static final String MODID = "lanternmod";
+    public static final String NAME = "Lantern Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
